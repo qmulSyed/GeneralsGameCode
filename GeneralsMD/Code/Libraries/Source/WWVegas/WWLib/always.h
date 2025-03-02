ddea1e4e9530a0a03d8dfc4784a922ac3ab6ebde
Patrick Zacharias <1475802+Fighter19@users.noreply.github.com>
2025-03-02 13:15:26 +0100

    WWLib: always.h: match function signatures in GameMemory.h

--- GeneralsMD/Code/Libraries/Source/WWVegas/WWLib/always.h
+++ GeneralsMD/Code/Libraries/Source/WWVegas/WWLib/always.h
@@ -78,23 +78,23 @@
 	#define _OPERATOR_NEW_DEFINED_
 
 	extern void * __cdecl operator new		(size_t size);
-	extern void __cdecl operator delete		(void *p);
+	extern void __cdecl operator delete		(void *p) noexcept;
 
 	extern void * __cdecl operator new[]	(size_t size);
-	extern void __cdecl operator delete[]	(void *p);
+	extern void __cdecl operator delete[]	(void *p) noexcept;
 
 	// additional overloads to account for VC/MFC funky versions
-	extern void* __cdecl operator new			(size_t nSize, const char *, int);
-	extern void __cdecl operator delete		(void *, const char *, int);
+	extern void* __cdecl operator new(size_t nSize, const char *, int);
+	extern void __cdecl operator delete(void *, const char *, int) noexcept;
 
-	extern void* __cdecl operator new[]		(size_t nSize, const char *, int);
-	extern void __cdecl operator delete[]	(void *, const char *, int);
+	extern void* __cdecl operator new[](size_t nSize, const char *, int);
+	extern void __cdecl operator delete[](void *, const char *, int) noexcept;
 
 	// additional overloads for 'placement new'
 	//inline void* __cdecl operator new							(size_t s, void *p) { return p; }
 	//inline void __cdecl operator delete						(void *, void *p)		{ }
-	inline void* __cdecl operator new[]						(size_t s, void *p) { return p; }
-	inline void __cdecl operator delete[]					(void *, void *p)		{ }
+	// inline void* __cdecl operator new[]						(size_t s, void *p) noexcept { return p; }
+	// inline void __cdecl operator delete[]					(void *, void *p) noexcept		{ }
 
 #endif
 
