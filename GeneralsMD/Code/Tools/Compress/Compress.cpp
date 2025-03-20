6d8a01a955f423cb9ff0735783d27ff04254c03d
Stephan Vedder <vedder@mbits.info>
2025-03-20 12:12:25 +0100

    Compress: Move compat code out of tool

--- GeneralsMD/Code/Tools/Compress/Compress.cpp
+++ GeneralsMD/Code/Tools/Compress/Compress.cpp
@@ -18,48 +18,10 @@
 
 #include <string>
 #include <cstdio>
+#include "always.h"
 #include "Lib/BaseType.h"
 #include "Compression/Compression.h"
 #include "../CRCDiff/debug.h"
-
-#ifdef _UNIX
-#include <alloca.h>
-#define _alloca alloca
-#include <ctype.h>
-#include <cstring>
-#include <wchar.h>
-typedef char TCHAR;
-typedef wchar_t WCHAR; 
-#define _tcslen strlen
-#define _tcsclen strlen
-#define _tcscmp strcmp
-#define _tcsicmp strcasecmp
-#define _wcsicmp wcscasecmp 
-#define stricmp strcasecmp
-#define strnicmp strncasecmp
-#define strcmpi strcasecmp
-
-#define _snprintf snprintf
-#define strupr(s) for(int i=0; i<strlen(s); i++) s[i]=toupper(s[i])
-char *strrev(char *str)
-{
-    if (!str || ! *str)
-        return str;
-
-    int i = strlen(str) - 1, j = 0;
-
-    char ch;
-    while (i > j)
-    {
-        ch = str[i];
-        str[i] = str[j];
-        str[j] = ch;
-        i--;
-        j++;
-    }
-    return str;
-}
-#endif
 
 #ifndef DEBUG
 
