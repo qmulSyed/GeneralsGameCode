72cff841915caf3e78ad37f71805e63260733283
Syed Ali Raza <98828911+qmulSyed@users.noreply.github.com>
2025-04-12 18:00:10 +0100

    Delete cmake directory

--- cmake/miles.cmake
+++
@@ -1,7 +0,0 @@
-FetchContent_Declare(
-    miles
-    GIT_REPOSITORY https://github.com/TheSuperHackers/miles-sdk-stub.git
-    GIT_TAG        0fef646a85c822475d55f19e3ca185263fb4a967
-)
-
-FetchContent_MakeAvailable(miles)
